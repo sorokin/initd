11345cbf7f3be1e55fdc47a69e83bf194d3ad2f8
Ivan Sorokin <vanyacpp@gmail.com>
2014-03-08 22:12:52 +0400

    fix compilability

--- libinitd-readconfig/read_value_node/read_value_node.cpp
+++ libinitd-readconfig/read_value_node/read_value_node.cpp
@@ -1,1 +1,25 @@
 #include "read_value_node.h"
+
+std::string get_type_by_value_node(value_node const& node)
+{
+    switch (node.get_type())
+    {
+    case value_node_type::integer:
+        return "int";
+    case value_node_type::string:
+        return "string";
+    case value_node_type::boolean:
+        return "bool";
+    case value_node_type::pseudo_identifier:
+        assert(false);
+        return "<pseudo identifier>";
+    case value_node_type::struct_:
+        {
+            struct_node const& cnode = static_cast<struct_node const&>(node);
+            return cnode.get_tag().get_text();
+        }
+    default:
+        assert(false);
+        return "<invalid value_node_type>";
+    }
+}
--- libinitd-readconfig/read_value_node/read_value_node.h
+++ libinitd-readconfig/read_value_node/read_value_node.h
@@ -31,29 +31,7 @@ bool can_read_value_node(value_node const& node)
     return read_value_node_impl<T>::can_read(node);
 }
 
-std::string get_type_by_value_node(value_node const& node)
-{
-    switch (node.get_type())
-    {
-    case value_node_type::integer:
-        return "int";
-    case value_node_type::string:
-        return "string";
-    case value_node_type::boolean:
-        return "bool";
-    case value_node_type::pseudo_identifier:
-        assert(false);
-        return "<pseudo identifier>";
-    case value_node_type::struct_:
-        {
-            struct_node const& cnode = static_cast<struct_node const&>(node);
-            return cnode.get_tag().get_text();
-        }
-    default:
-        assert(false);
-        return "<invalid value_node_type>";
-    }
-}
+std::string get_type_by_value_node(value_node const& node);
 
 template <>
 struct read_value_node_impl<int>
